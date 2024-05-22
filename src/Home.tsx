d70f208f2356f6fc7a92b7d5133751030b7924e9
unknown <benedictusbangkit@gmail.com>
2024-05-22 13:51:43 +0700

    remove skillcard Home page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,7 @@
 import Navbar from "./component/navbar";
 import Banner from "./routes/home/banner";
 import About from "./routes/home/about";
-import SkillCard from "./component/skillCard";
+
 function Home(){
     return(
         <>
